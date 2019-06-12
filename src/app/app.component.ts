00e9a0301975b0869ae0af91d6a7496a12d62b89
Billy <tb.mokolokoto@gmail.com>
2019-06-12 11:29:28 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Array';
+  tasklList = [{task:'',priority:''}];
+  newTask:string ="";
+  NewPriority:string="";
+
+  addList() {
+  this.tasklList.push({task:this.newTask,priority:this.NewPriority})
+
+  this.newTask ='';
+  this.NewPriority='';
 }
+
+delete(list){
+let index= this.tasklList.indexOf(list)
+this.tasklList.splice(index,1)
+}
+
+
+edit(list,newTask,NewPriority){
+  let index= this.tasklList.indexOf(list)
+  let taskPrompt = prompt("Edit Task",list.task)
+  let prioPrompt = prompt("Edit Priority",list.priority)
+  this.tasklList[index].task = taskPrompt
+  this.tasklList[index].priority = prioPrompt
+
+if (taskPrompt ==null || taskPrompt =="" ){
+  this.tasklList= list
+
+}
+
+if(prioPrompt ==null || prioPrompt ==""){
+this.tasklList = list
+
+}
+
+}
+
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
